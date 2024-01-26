6f24e83c3839281637e0f339eb5e4e14755499c1
Sam Saju <2618654s@student.gla.ac.uk>
2024-01-26 12:30:02 +0000

    Actually finished Chp 6 Exercises

--- rango/views.py
+++ rango/views.py
@@ -6,10 +6,12 @@ from rango.models import Page
 # Create your views here.
 def index(request):
     category_list = Category.objects.order_by('-likes')[:5]
+    most_viewed = Page.objects.order_by('views')[:5]
 
     context_dict = {}
     context_dict['boldmessage'] = 'Crunchy, creamy, cookie, candy, cupcake!'
     context_dict['categories'] = category_list
+    context_dict['most_viewed'] = most_viewed
 
     return render(request, 'rango/index.html', context=context_dict)
 
@@ -22,10 +24,11 @@ def show_category(request, category_name_slug):
 
     try:
         category = Category.objects.get(slug=category_name_slug)
-        pages = Page.objects.filter(category=category)
         
+        pages = Page.objects.filter(category=category)
         context_dict['pages'] = pages
         context_dict['category'] = category
+        
     except Category.DoesNotExist:
         context_dict['category'] = None
         context_dict['pages'] = None
